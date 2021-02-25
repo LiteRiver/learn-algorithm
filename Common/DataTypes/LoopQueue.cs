246aaa1667acbe3ba69aa2a9d76e32674e9f6c5f
clive <zhxchchuan@gmail.com>
2021-02-25 14:16:25 +0800

    init LoopQueue

---
+++ Common/DataTypes/LoopQueue.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Common.DataTypes {
+  public class LoopQueue<T> : IQueue<T> {
+    public int Size => throw new NotImplementedException();
+
+    public bool IsEmpty => throw new NotImplementedException();
+
+    public T Front => throw new NotImplementedException();
+
+    public T Dequeue() {
+      throw new NotImplementedException();
+    }
+
+    public void Enqueue(T el) {
+      throw new NotImplementedException();
+    }
+  }
+}
